73fabc7cab5250cc81610c573ddcb6c1014bb69d
Marker-bit <maarrrkkkk@ya.ru>
2025-06-12 11:33:09 +0300

    add a lot of mistral models

--- lib/models.ts
+++ lib/models.ts
@@ -226,7 +226,156 @@ const models: Model[] = [
     supportsTools: true,
   },
   {
-    id: "magistral-small",
+    id: "magistral-medium",
+    title: "Magistral Small",
+    model: "Magistral",
+    version: "Small",
+    providers: {
+      openrouter: {
+        modelName: "mistralai/magistral-medium-2506",
+        features: ["reasoning"],
+      },
+      mistral: {
+        modelName: "magistral-medium-2506",
+        features: ["reasoning"],
+      },
+    },
+    icon: MistralAI,
+    supportsTools: false,
+  },
+  {
+    id: "mistral-medium",
+    title: "Mistral Medium",
+    model: "Mistral",
+    version: "Medium",
+    providers: {
+      openrouter: {
+        modelName: "mistralai/mistral-medium-2505",
+        features: ["reasoning"],
+      },
+      mistral: {
+        modelName: "mistral-medium-2505",
+        features: ["reasoning"],
+      },
+    },
+    icon: MistralAI,
+    supportsTools: true,
+  },
+  {
+    id: "codestral-2501",
+    title: "Codestral",
+    model: "Codestral",
+    version: "",
+    additionalTitle: "2501",
+    providers: {
+      openrouter: {
+        modelName: "mistralai/codestral-2501",
+        features: ["reasoning"],
+      },
+      mistral: {
+        modelName: "codestral-2501",
+        features: ["reasoning"],
+      },
+    },
+    icon: MistralAI,
+    supportsTools: false,
+  },
+  {
+    id: "mistral-saba-2502",
+    title: "Mistral Saba",
+    model: "Mistral",
+    version: "Saba",
+    providers: {
+      openrouter: {
+        modelName: "mistralai/mistral-saba-2502",
+        features: ["reasoning"],
+      },
+      mistral: {
+        modelName: "mistral-saba-2502",
+        features: ["reasoning"],
+      },
+    },
+    icon: MistralAI,
+    supportsTools: false,
+  },
+  {
+    id: "mistral-large-2411",
+    title: "Mistral Large",
+    model: "Mistral",
+    version: "Large",
+    additionalTitle: "2411",
+    providers: {
+      openrouter: {
+        modelName: "mistralai/mistral-large-2411",
+        features: ["reasoning"],
+      },
+      mistral: {
+        modelName: "mistral-large-2411",
+        features: ["reasoning"],
+      },
+    },
+    icon: MistralAI,
+    supportsTools: false,
+  },
+  {
+    id: "pixtral-large-2411",
+    title: "Pixtral Large",
+    model: "Pixtral",
+    version: "Large",
+    additionalTitle: "2411",
+    providers: {
+      openrouter: {
+        modelName: "mistralai/pixtral-large-2411",
+        features: ["reasoning", "vision"],
+      },
+      mistral: {
+        modelName: "pixtral-large-2411",
+        features: ["reasoning", "vision"],
+      },
+    },
+    icon: MistralAI,
+    supportsTools: false,
+  },
+  {
+    id: "ministral-3b-2410",
+    title: "Ministral",
+    model: "Ministral",
+    version: "",
+    additionalTitle: "3B",
+    providers: {
+      openrouter: {
+        modelName: "mistralai/ministral-3b-2410",
+        features: ["reasoning"],
+      },
+      mistral: {
+        modelName: "ministral-3b-2410",
+        features: ["reasoning"],
+      },
+    },
+    icon: MistralAI,
+    supportsTools: false,
+  },
+  {
+    id: "ministral-8b-2410",
+    title: "Ministral",
+    model: "Ministral",
+    version: "",
+    additionalTitle: "8B",
+    providers: {
+      openrouter: {
+        modelName: "mistralai/ministral-8b-2410",
+        features: ["reasoning"],
+      },
+      mistral: {
+        modelName: "ministral-8b-2410",
+        features: ["reasoning"],
+      },
+    },
+    icon: MistralAI,
+    supportsTools: false,
+  },
+  {
+    id: "magistral-small-2506",
     title: "Magistral Small",
     model: "Magistral",
     version: "Small",
@@ -238,6 +387,62 @@ const models: Model[] = [
       mistral: {
         modelName: "magistral-small-2506",
         features: ["reasoning"],
+      },
+    },
+    icon: MistralAI,
+    supportsTools: false,
+  },
+  {
+    id: "devstral-small-2506",
+    title: "Devstral Small",
+    model: "Devstral",
+    version: "Small",
+    providers: {
+      openrouter: {
+        modelName: "mistralai/devstral-small-2506",
+        features: ["reasoning"],
+      },
+      mistral: {
+        modelName: "devstral-small-2506",
+        features: ["reasoning"],
+      },
+    },
+    icon: MistralAI,
+    supportsTools: false,
+  },
+  {
+    id: "mistral-small-2503",
+    title: "Mistral Small",
+    model: "Mistral",
+    version: "Small",
+    additionalTitle: "2503",
+    providers: {
+      openrouter: {
+        modelName: "mistralai/mistral-small-2503",
+        features: ["reasoning"],
+      },
+      mistral: {
+        modelName: "mistral-small-2503",
+        features: ["reasoning"],
+      },
+    },
+    icon: MistralAI,
+    supportsTools: false,
+  },
+  {
+    id: "pixtral-12b-2409",
+    title: "Pixtral",
+    model: "Pixtral",
+    version: "",
+    additionalTitle: "2409",
+    providers: {
+      openrouter: {
+        modelName: "mistralai/pixtral-12b-2409",
+        features: ["reasoning", "vision"],
+      },
+      mistral: {
+        modelName: "pixtral-12b-2409",
+        features: ["reasoning", "vision"],
       },
     },
     icon: MistralAI,
